443429b5b0703f31171ed1ff49e57e3bf6c1487b
Jakub Domaszewicz <domaszewicz@insalgo.com>
2018-04-14 11:14:44 +0200

    Initial design

---
+++ ios-app/ihs-hackathon-hs2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ihs-hackathon-hs2
+//
+//  Created by Kuba Domaszewicz on 14.04.2018.
+//  Copyright © 2018 hs2. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
